1cb3b0e55060e0a482def642ea060a60b47f3681
ggocodelab <brgillian@gmail.com>
2025-12-24 10:55:07 -0300

    chore: initial spring boot project setup

---
+++ src/main/java/com/ggocodelab/ds_challenge_3/DsChallenge3Application.java
@@ -0,0 +1,13 @@
+package com.ggocodelab.ds_challenge_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsChallenge3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsChallenge3Application.class, args);
+	}
+
+}
